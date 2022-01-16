a3572e69657d3cc4a0ffe9480d987046b1af6d20
AlexandrRumiantsev <r-sasha@list.ru>
2022-01-16 14:03:31 +0300

    Добавили сервер

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-// run `node index.js` in the terminal
+//console.log(`Hello Node.js v${process.versions.node}!`);
 
-console.log(`Hello Node.js v${process.versions.node}!`);
-console.log('TEST')
-console.log('TEST2')
+// 1 ШАГ. Пoдключить express
+const express = require('express');
+
+// 2 ШАГ. Создаем объект приложения
+const app = express();
+
+// 4 ШАГ. Создаем первый роут приложения
+// 1 параметр - адрес страницы на которую мы переходим
+app.get('/', function (req, res) {
+  res.send('<h1>Hello SERVER</h1>');
+});
+// 3 ШАГ. Запуск приложение на определенном порту
+app.listen(3000);
